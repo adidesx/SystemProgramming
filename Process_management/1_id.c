389c8f371e710126974b74805e497d2720ef0e38
Aditya Deshpande <Aditya.Deshpande@harman.com>
2017-12-27 10:48:55 +0000

    Process id program

---
+++ Process_management/1_id.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main()
+{
+	printf("Current Process ID : %d\n", getpid());
+	printf("Parent Process ID : %d\n", getppid());
+	printf("Group  Process ID : %d\n", getpgrp());
+	return 0;
+}
