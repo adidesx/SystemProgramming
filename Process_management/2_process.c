72023b5724b6f3a1956e1b3142b8ff5596bf75f2
Aditya Deshpande <Aditya.Deshpande@harman.com>
2017-12-27 15:53:33 +0000

    Process Program

---
+++ Process_management/2_process.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+	printf("Main Start\n");
+	int f = fork();
+	if(f == 0) {
+		printf("Child : %d\n", getpid());
+	} else {
+		printf("Parent : %d\n", getpid());
+	}
+	return 0;	
+}
+
